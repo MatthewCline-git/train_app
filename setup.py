424fb8d68503cacd998358a5ab65661cdbf315bd
MatthewCline-git <matthewcline1028@gmail.com>
2024-11-30 16:32:26 -0500

    Relocate csv file

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setup(
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    package_data={
+        "mta_api": ["data/*.csv"]
+    },
     install_requires=[line.strip() for line in open("requirements.txt")],
 )
